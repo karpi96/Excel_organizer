eebd8d1cd8ac668030f15b9496d5ba70f5d7e5a4
karpi <arpikocsis@gmail.com>
2020-08-25 00:45:45 +0200

    reads keyword from excel file, and writes them to .txt file

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import os
+import openpyxl
 
 keyword = input("enter a keyword: \n")
+os.chdir("/home/karpi/Documents/excelsample/")
+
 
 directory = os.fsencode("/home/karpi/Documents/excelsample/")
-    
+
+file_name = open("SearchResult.txt","w")
+
+#go through files in a directory
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
+
     if filename.endswith(".xlsx") or filename.endswith(".xlsm"):
-        # print(os.path.join(directory, filename))
+        
+        #read excel file
         wb = openpyxl.load_workbook(filename)
         sheet = wb.get_sheet_by_name('Sheet1')
         for i in range(1,10):
             for j in range(1,10):
                 if sheet.cell(row = i, column = j).value == keyword:
-                print("keywoard is in document")
+                    file_name.write(filename)
+                    break
+            else:
+                continue
+            break
                 
         continue
     else:
